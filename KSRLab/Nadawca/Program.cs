a511ff5def67b1e0259138f02be3ed5e3e5ae7ac
Lewap333 <pkusznierczuk99@gmail.com>
2025-04-22 15:17:56 +0200

    Nadawca + Odbiorca

---
+++ KSRLab/Nadawca/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nadawca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "kebnekaise.lmq.cloudamqp.com",
+                VirtualHost = "derzxpib",
+                UserName = "derzxpib",
+                Password = "41TnllDwLvifJRxipPALriNKQfTGq7Hx",
+                Port = 5672,
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("message_queue", false, false, false, null);
+
+                for (int i = 0; i < 10; i++)
+                {
+
+                    string text = $"Wiadomość {i} od Nadawcy";
+                    var body = Encoding.UTF8.GetBytes(text);
+
+                    channel.BasicPublish("", "message_queue", null, body);
+                }
+                Console.WriteLine("Wiadomości wysłane");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ KSRLab/Odbiorca/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Xml.Linq;
+
+
+namespace Odbiorca
+{
+    class MyConsumer : DefaultBasicConsumer
+    {
+        public MyConsumer(IModel model) : base(model)
+        {
+        }
+
+        public override void HandleBasicDeliver(
+        string consumerTag,
+        ulong deliveryTag,
+        bool redelivered,
+        string exchange,
+        string routingKey,
+        IBasicProperties properties,
+        byte[] body)
+        {
+          
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Odebrana wiadomość: {message}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "kebnekaise.lmq.cloudamqp.com",
+                VirtualHost = "derzxpib",
+                UserName = "derzxpib",
+                Password = "41TnllDwLvifJRxipPALriNKQfTGq7Hx",
+                Port = 5672,
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("message_queue", false, false, false, null);
+
+                var odbiorca = new MyConsumer(channel);
+
+                channel.BasicConsume("message_queue", false, odbiorca);
+
+                Console.WriteLine("Odbiorca nasłuchuje na wiadomości");
+                Console.ReadKey();
+            }
+        }
+    }
+}
